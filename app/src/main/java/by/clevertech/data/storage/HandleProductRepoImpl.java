a9685f1bb6ac16b6a4fbfc94738a0aeffd6da426
Nikita Semeniuk <nikstannum@yandex.ru>
2022-12-18 20:07:02 +0300

    update HandleProdRepoImpl

--- app/src/main/java/by/clevertech/data/storage/HandleProductRepoImpl.java
+++ app/src/main/java/by/clevertech/data/storage/HandleProductRepoImpl.java
@@ -24,85 +24,96 @@ public class HandleProductRepoImpl implements ProductRepository {
     }
 
     private Map<Long, Product> initRepoStub() {
-        Map<Long, Product> map = new HashMap<>();
+        Map<Long, Product> repo = new HashMap<>();
         Product p1 = new Product();
-        p1.setId(getIdSequence());
+        Long id1 = getIdSequence();
+        p1.setId(id1);
         p1.setName("milk");
         p1.setPrice(BigDecimal.valueOf(0.99));
         p1.setDiscount(false);
-        repo.put(1L, p1);
+        repo.put(id1, p1);
 
         Product p2 = new Product();
-        p2.setId(getIdSequence());
+        Long id2 = getIdSequence();
+        p2.setId(id2);
         p2.setName("yogurt");
         p2.setPrice(BigDecimal.valueOf(0.87));
         p2.setDiscount(false);
-        repo.put(2L, p2);
+        repo.put(id2, p2);
 
         Product p3 = new Product();
-        p3.setId(getIdSequence());
+        Long id3 = getIdSequence();
+        p3.setId(id3);
         p3.setName("bread");
         p3.setPrice(BigDecimal.valueOf(0.7));
         p3.setDiscount(false);
-        repo.put(3L, p3);
+        repo.put(id3, p3);
 
         Product p4 = new Product();
-        p4.setId(getIdSequence());
+        Long id4 = getIdSequence();
+        p4.setId(id4);
         p4.setName("cookie");
         p4.setPrice(BigDecimal.valueOf(1));
         p4.setDiscount(true);
-        repo.put(4L, p4);
+        repo.put(id4, p4);
 
         Product p5 = new Product();
-        p5.setId(getIdSequence());
+        Long id5 = getIdSequence();
+        p5.setId(id5);
         p5.setName("butter");
         p5.setPrice(BigDecimal.valueOf(1.05));
         p5.setDiscount(false);
-        repo.put(5L, p5);
+        repo.put(id5, p5);
 
         Product p6 = new Product();
-        p6.setId(getIdSequence());
+        Long id6 = getIdSequence();
+        p6.setId(id6);
         p6.setName("deodorant");
         p6.setPrice(BigDecimal.valueOf(2.18));
         p6.setDiscount(true);
-        repo.put(6L, p6);
+        repo.put(id6, p6);
 
         Product p7 = new Product();
-        p7.setId(getIdSequence());
+        Long id7 = getIdSequence();
+        p7.setId(id7);
         p7.setName("soap");
         p7.setPrice(BigDecimal.valueOf(0.89));
         p7.setDiscount(true);
-        repo.put(7L, p7);
+        repo.put(id7, p7);
 
         Product p8 = new Product();
-        p8.setId(getIdSequence());
+        Long id8 = getIdSequence();
+        p8.setId(id8);
         p8.setName("shower gel");
         p8.setPrice(BigDecimal.valueOf(2.94));
         p8.setDiscount(true);
-        repo.put(8L, p8);
+        repo.put(id8, p8);
 
         Product p9 = new Product();
-        p9.setId(getIdSequence());
+        Long id9 = getIdSequence();
+        p9.setId(id9);
         p9.setName("toothbrush");
         p9.setPrice(BigDecimal.valueOf(1.03));
         p9.setDiscount(true);
-        repo.put(9L, p9);
+        repo.put(id9, p9);
 
         Product p10 = new Product();
-        p10.setId(getIdSequence());
+        Long id10 = getIdSequence();
+        p10.setId(id10);
         p10.setName("toothpaste");
         p10.setPrice(BigDecimal.valueOf(1.79));
         p10.setDiscount(true);
-        repo.put(10L, p10);
+        repo.put(id10, p10);
 
         Product p11 = new Product();
-        p11.setId(getIdSequence());
+        Long id11 = getIdSequence();
+        p11.setId(id11);
         p11.setName("candies");
         p11.setPrice(BigDecimal.valueOf(3.18));
         p11.setDiscount(true);
-        repo.put(11L, p11);
+        repo.put(id11, p11);
 
-        return map;
+        return repo;
     }
 
     @Override
